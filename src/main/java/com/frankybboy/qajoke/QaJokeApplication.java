f8884794a37aa013bcc98297f2fd9d56b41e8e9b
FrankyBBoy <francis1592@gmai.com>
2021-09-23 15:18:27 -0400

    First commit

---
+++ src/main/java/com/frankybboy/qajoke/QaJokeApplication.java
@@ -0,0 +1,13 @@
+package com.frankybboy.qajoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaJokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frankybboy/qajoke/QaJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankybboy.qajoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
