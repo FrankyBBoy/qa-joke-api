4ffbedb17d3f3f04ae5e67149e5b49bd23f0ab8f
FrankyBBoy <francis1592@gmai.com>
2021-09-24 13:51:31 -0400

    Add Integration Test on service. Closes #1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # qa-joke-api
 Simple question&amp;answer joke API
+
+## Prerequisite
+- JDK 11
+- Maven 3.8.1+
+- IntelliJ plugins: lombok, mapstruct
---
+++ src/test/java/com/frankybboy/qajoke/service/JokeServiceImplIT.java
@@ -0,0 +1,145 @@
+package com.frankybboy.qajoke.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.frankybboy.qajoke.dto.JokeDto;
+import com.frankybboy.qajoke.mapper.JokeMapperImpl;
+import com.frankybboy.qajoke.model.Joke;
+import com.frankybboy.qajoke.repository.JokeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@DataJpaTest
+public class JokeServiceImplIT {
+
+    private static final Long JOKE_ID_1 = 1L;
+    private static final String JOKE_QUESTION_1 = "My question 1?";
+    private static final String JOKE_ANSWER_1 = "My answer 1!";
+    private static final String JOKE_QUESTION_2 = "My question 2?";
+    private static final String JOKE_ANSWER_2 = "My answer 2!";
+
+    Joke simpleJoke1;
+    Joke simpleJoke2;
+
+    @Autowired
+    JokeRepository jokeRepository;
+
+    JokeService jokeService;
+
+    @BeforeEach
+    void setup() {
+        simpleJoke1 = Joke.builder().question(JOKE_QUESTION_1).answer(JOKE_ANSWER_1).build();
+        simpleJoke2 = Joke.builder().question(JOKE_QUESTION_2).answer(JOKE_ANSWER_2).build();
+
+        jokeService = new JokeServiceImpl(jokeRepository, new JokeMapperImpl());
+    }
+
+    @Test
+    public void getAllJokes() {
+        // given
+        jokeRepository.save(simpleJoke1);
+        jokeRepository.save(simpleJoke2);
+
+        // when
+        List<JokeDto> result = jokeService.getAllJokes();
+
+        // then
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals(JOKE_QUESTION_1, result.get(0).getQuestion());
+        assertEquals(JOKE_ANSWER_1, result.get(0).getAnswer());
+    }
+
+    @Test
+    public void getJokeById() {
+        // given
+        Joke savedJoke = jokeRepository.save(simpleJoke1);
+
+        // when
+        JokeDto result = jokeService.getJokeById(savedJoke.getId());
+
+        // then
+        assertNotNull(result);
+        assertEquals(JOKE_QUESTION_1, result.getQuestion());
+        assertEquals(JOKE_ANSWER_1, result.getAnswer());
+    }
+
+    @Test
+    public void getJokeByIdNotFound() {
+        // given
+        Long nonExistentId = 54321L;
+
+        // when
+        Exception result = assertThrows(ResponseStatusException.class, () -> {
+            jokeService.getJokeById(nonExistentId);
+        });
+
+        // then
+        assertNotNull(result);
+        assertEquals("404 NOT_FOUND \"Joke not found\"", result.getMessage());
+    }
+
+    @Test
+    public void createJoke() {
+        // given
+        int actualSize = jokeRepository.findAll().size();
+        int expectedSize = actualSize + 1;
+
+        // when
+        jokeService.createJoke(JokeDto.builder().build());
+
+        // then
+        assertEquals(expectedSize, jokeRepository.findAll().size());
+    }
+
+    @Test
+    public void saveJoke() {
+        // given
+        Joke jokeToUpdate = jokeRepository.save(simpleJoke1);
+        String question = "Updated question?";
+        String answer = "Updated answer!";
+        JokeDto jokeDto = JokeDto.builder().question(question).answer(answer).build();
+
+        // when
+        JokeDto savedJoke = jokeService.saveJoke(jokeToUpdate.getId(), jokeDto);
+
+        // then
+        assertNotNull(savedJoke);
+        assertEquals(question, savedJoke.getQuestion());
+        assertEquals(answer, savedJoke.getAnswer());
+    }
+
+    @Test
+    public void patchJoke() {
+        // given
+        Joke jokeToPatch = jokeRepository.save(Joke.builder().question(JOKE_QUESTION_1).answer(JOKE_ANSWER_1).build());
+        String patchedQuestion = "Patched question?";
+        JokeDto jokeDto = JokeDto.builder().question(patchedQuestion).build();
+
+        // when
+        JokeDto savedJoke = jokeService.patchJoke(jokeToPatch.getId(), jokeDto);
+
+        // then
+        assertNotNull(savedJoke);
+        assertEquals(patchedQuestion, savedJoke.getQuestion());
+        assertEquals(JOKE_ANSWER_1, savedJoke.getAnswer());
+    }
+
+    @Test
+    public void deleteJokeById() {
+        // given
+        Joke savedJoke = jokeRepository.save(simpleJoke1);
+        int expectedSize = jokeRepository.findAll().size() - 1;
+
+        // when
+        jokeService.deleteJokeById(savedJoke.getId());
+
+        // then
+        assertEquals(expectedSize, jokeRepository.findAll().size());
+    }
+}
